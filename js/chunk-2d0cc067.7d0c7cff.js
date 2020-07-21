e0ea9d74e5922049855992d725e6096f4e20d569
sva71 <crockosha@gmil.com>
2020-07-21 20:11:32 +0300

    Todo-list Vue.js added

---
+++ js/chunk-2d0cc067.7d0c7cff.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cc067"],{"4bbe":function(e,n,a){"use strict";a.r(n);var t=function(){var e=this,n=e.$createElement,a=e._self._c||n;return a("b-alert",{attrs:{show:"",variant:"danger"}},[e._v("Запрашиваемая Вами страница не найдена!")])},r=[],c={name:"P404"},s=c,u=a("2877"),l=Object(u["a"])(s,t,r,!1,null,"3ab4af41",null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0cc067.7d0c7cff.js.map
